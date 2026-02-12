ce936dd3a473b9c930162525871643ebf9825367
unknown <vladimiraurelius@gmail.com>
2026-02-12 19:47:42 +0200

    add html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './sass/main.scss';
 
 function App() {
-	return <h1>Hello world</h1>;
+	return (
+		<div className="container">
+			<div className="header">
+				<h1>Mafia</h1>
+				<nav>
+					<ul>
+						<li>
+							<button>Игроки</button>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div className="main">
+				<ul className="players">
+					<li className="player">
+						<span>1</span>
+						<p>Peter</p>
+						<p>Role</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
