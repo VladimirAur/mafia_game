6f04ba51bf0fe14642e8ec2cf7e9d95bdd521fb5
Aurelius <vladimiraurelius@gmail.com>
2026-02-13 16:33:39 +0200

    add players

--- src/App.js
+++ src/App.js
@@ -12,6 +12,25 @@ function App() {
 					<button className="header__nav-item">Результат</button>
 				</nav>
 			</div>
+			<div className="players">
+				<ul className="players__list">
+					<li className="player">
+						<span className="player__number">1</span>
+						<div className="player__desc">
+							<div className="player__name">Купер</div>
+							<div className="player__status">
+								<div className="player__role">М</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn">-</button>
+									<input type="text" className="player__foll-count" />
+									<button className="player__foll-btn">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
