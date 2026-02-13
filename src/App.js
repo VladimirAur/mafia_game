13237dc6f4b3d423db5e30383aad680b3028831b
Aurelius <vladimiraurelius@gmail.com>
2026-02-13 18:29:29 +0200

    presentation

--- src/App.js
+++ src/App.js
@@ -15,16 +15,151 @@ function App() {
 			<div className="players">
 				<ul className="players__list">
 					<li className="player">
-						<span className="player__number">1</span>
-						<div className="player__desc">
-							<div className="player__name">Купер</div>
+						<span className="player__number player__number--default">1</span>
+						<div className="player__desc player__desc--default">
+							<div className="player__name">Молотов</div>
+							<div className="player__status">
+								<div className="player__role">Ж</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="1" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player">
+						<span className="player__number player__number--black">2</span>
+						<div className="player__desc player__desc--black">
+							<div className="player__name">Алиас</div>
 							<div className="player__status">
 								<div className="player__role">М</div>
 								<div className="player__foll">
 									<div className="player__foll-name">Фол</div>
-									<button className="player__foll-btn">-</button>
-									<input type="text" className="player__foll-count" />
-									<button className="player__foll-btn">+</button>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="0" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player">
+						<span className="player__number player__number--default">3</span>
+						<div className="player__desc player__desc--default">
+							<div className="player__name">Игрокер</div>
+							<div className="player__status">
+								<div className="player__role">Ж</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="2" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player">
+						<span className="player__number player__number--default">4</span>
+						<div className="player__desc player__desc--default">
+							<div className="player__name">Никитос</div>
+							<div className="player__status">
+								<div className="player__role">Ж</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="0" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player">
+						<span className="player__number player__number--red">5</span>
+						<div className="player__desc player__desc--red">
+							<div className="player__name">Купер</div>
+							<div className="player__status">
+								<div className="player__role">Ш</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="0" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player">
+						<span className="player__number player__number--black">6</span>
+						<div className="player__desc player__desc--black">
+							<div className="player__name">Пижон</div>
+							<div className="player__status">
+								<div className="player__role">М</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="0" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player">
+						<span className="player__number player__number--red">7</span>
+						<div className="player__desc player__desc--red">
+							<div className="player__name">Сантехник</div>
+							<div className="player__status">
+								<div className="player__role">Д</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="0" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player">
+						<span className="player__number player__number--default">8</span>
+						<div className="player__desc player__desc--default">
+							<div className="player__name">Нейминг</div>
+							<div className="player__status">
+								<div className="player__role">Ж</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="1" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player player--empty">
+						<span className="player__number player__number--default">9</span>
+						<div className="player__desc player__desc--default">
+							<div className="player__name">Ахилес</div>
+							<div className="player__status">
+								<div className="player__role">Ж</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="1" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
+								</div>
+							</div>
+						</div>
+					</li>
+					<li className="player">
+						<span className="player__number player__number--default">10</span>
+						<div className="player__desc player__desc--default">
+							<div className="player__name">Замод</div>
+							<div className="player__status">
+								<div className="player__role">Ж</div>
+								<div className="player__foll">
+									<div className="player__foll-name">Фол</div>
+									<button className="player__foll-btn player__foll-right">-</button>
+									<input type="text" defaultValue="1" className="player__foll-count" />
+									<button className="player__foll-btn player__foll-left">+</button>
 								</div>
 							</div>
 						</div>
