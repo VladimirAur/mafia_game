96144bedab933ac3a6cfef8bf3f6bb722ace7aa2
unknown <vladimiraurelius@gmail.com>
2026-02-13 12:08:05 +0200

    add prettier

--- src/App.js
+++ src/App.js
@@ -2,26 +2,9 @@ import './sass/main.scss';
 
 function App() {
 	return (
-		<div className="container">
-			<div className="header">
-				<h1>Mafia</h1>
-				<nav>
-					<ul>
-						<li>
-							<button>Игроки</button>
-						</li>
-					</ul>
-				</nav>
-			</div>
-			<div className="main">
-				<ul className="players">
-					<li className="player">
-						<span>1</span>
-						<p>Peter</p>
-						<p>Role</p>
-					</li>
-				</ul>
-			</div>
+		<div className="post">
+			<div className="post__photo"></div>
+			<div className="post__text"></div>
 		</div>
 	);
 }
