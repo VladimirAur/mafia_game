eaa2c39e0e44828f9dfcc586d4a5e3c61d0eaee6
unknown <vladimiraurelius@gmail.com>
2026-02-13 13:11:32 +0200

    add color scheme

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './sass/main.scss';
 function App() {
 	return (
 		<div className="container">
-			<div className="post">
-				<div className="post__photo">www</div>
-				<div className="post__text">ee</div>
+			<div className="header">
+				<h2 className="header__name">Mafia</h2>
+				<nav className="header__nav">
+					<button className="header__nav-item">Игроки</button>
+					<button className="header__nav-item">Голосование</button>
+					<button className="header__nav-item">Результат</button>
+				</nav>
 			</div>
 		</div>
 	);
