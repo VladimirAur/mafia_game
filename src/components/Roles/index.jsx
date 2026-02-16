efda0d1d270a5e16b7fd889a8b40b09575cba029
unknown <vladimiraurelius@gmail.com>
2026-02-16 21:55:58 +0200

    add more roles

--- src/components/Roles/index.jsx
+++ src/components/Roles/index.jsx
@@ -4,7 +4,8 @@ import object from '../../bd.json';
 
 const Roles = () => {
 	const [roles, setRoles] = React.useState([]);
-    const [activePopup, setActivePopup] = React.useState(false);
+    const [adding, setAdding] = React.useState(false);
+    const [isFocused, setIsFocused] = React.useState(false);
 
 	React.useEffect(() => {
 		setRoles(object.roles);
@@ -18,15 +19,29 @@ const Roles = () => {
 					<RolesItem key={item.id} {...item} />
 				))}
 			</ul>
-            {activePopup ? (
+            {adding ? (
                 <div className="roles__popup">
-				<input type="text" placeholder="Роль..." className="role__name" />
-				<button className="roles__add roles__add--small">Добавить</button>
-			</div>            
-            ):
-            (<button className="roles__add">+ Добавить роль</button>)
-        }
-            
+				<input 
+                    type="text"
+                    placeholder="Роль..." 
+                    className="role__name" 
+                    onFocus={() => setIsFocused(!isFocused)}
+                    onBlur={() => setIsFocused(false)} 
+                />                 
+				<button className="roles__add roles__add--small" onClick={()=> setAdding(!adding)}>Добавить</button>
+                </div>            
+                ):
+                (<button className="roles__add" onClick={()=> setAdding(!adding)}>+ Добавить роль</button>)
+            }
+            {isFocused && (
+                <ul className="roles__list">
+				{roles.map((item) => (
+					<RolesItem key={item.id} {...item} />
+				))}
+                </ul>
+                )
+
+            }
 			
 			
 			<button className="roles__start">Начать (10)</button>
