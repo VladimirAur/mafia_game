3d1a52430d0f02b6c3ec1098545016ff5f24e386
Aurelius <vladimiraurelius@gmail.com>
2026-02-17 15:22:27 +0200

    reset role

--- src/components/Roles/RolesItem.jsx
+++ src/components/Roles/RolesItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const RolesItem = ({ role, index, removeRole }) => {
+const RolesItem = ({ role, index, resetRole}) => {
 	const {name, number} = role;
 	return (
 		<li className="role">
@@ -9,7 +9,7 @@ const RolesItem = ({ role, index, removeRole }) => {
 				<button className="role__btn role__btn--left">-</button>
 				<div className="role__count-number">{number}</div>
 				<button className="role__btn role__btn--right">+</button>
-                <button className="role__btn role__btn--del" onClick={() => removeRole(index)}>x</button>
+                <button className="role__btn role__btn--del" onClick={() => resetRole(index)}>x</button>
 			</div>
 		</li>
 	);
--- src/components/Roles/index.jsx
+++ src/components/Roles/index.jsx
@@ -4,69 +4,85 @@ import object from '../../bd.json';
 
 const Roles = () => {
 	const [roles, setRoles] = React.useState([]);
-    const [adding, setAdding] = React.useState(false);
-    const [isFocused, setIsFocused] = React.useState(false);
-    const [value, setValue] = React.useState();
+	const [adding, setAdding] = React.useState(false);
+	const [isFocused, setIsFocused] = React.useState(false);
+	const [value, setValue] = React.useState();
 
 	React.useEffect(() => {
 		setRoles(object.roles);
 	}, []);
 
-    const addNewRole = (value) => {
-        const newRole ={
-            name: value,
-            number: 1
-        }
-        setRoles(prevRoles => [...prevRoles, newRole]);             
-        setAdding(false);
-        setValue("");
-    }
+	const addNewRole = (value) => {
+		const newRole = {
+			name: value,
+			number: 1,
+		};
+		setRoles((prevRoles) => [...prevRoles, newRole]);
+		setAdding(false);
+		setValue('');
+	};
 
-    const removeRole = (indexToRemove) =>{
-        setRoles(prevRoles => prevRoles.filter((_, index) => index !== indexToRemove));
-        
-    }
-
+	const resetRole = (indexToReset) => {
+		setRoles((prevRoles) =>
+			prevRoles.map(
+				(role, index) =>
+					index === indexToReset
+						? { ...role, number: 0 }
+						: role, 
+			),
+		);
+	};
 
 	return (
 		<div className="roles">
 			<h2 className="roles__title"># Настройки игры</h2>
 			<ul className="roles__list">
 				{roles.map((role, index) => (
-					<RolesItem 
-                        key={index} 
-                        role={role} 
-                        index={index} 
-                        removeRole={removeRole}/>
+					<RolesItem key={index} role={role} index={index} resetRole={resetRole} />
 				))}
 			</ul>
-            {adding ? (
-                <div className="roles__popup">
-                    <div className={`roles__select ${isFocused ? "roles__select--focus" : ""}`}>
-                        <input 
-                            type="text"
-                            value={value}
-                            placeholder="Роль..." 
-                            className={`role__name ${isFocused ? "role__name--modbrt" : ""}`} 
-                            onFocus={() => setIsFocused(!isFocused)}
-                            onBlur={() => setIsFocused(false)} 
-                            onChange={(e) => setValue(e.target.value)}
-                        /> 
-                        <span className="roles__close" onMouseDown={() => setValue("")}>x</span>
-                        {isFocused && (
-                            <ul className="roles__list">
-                                <li className="role__name role__name--modbrb" onMouseDown={() => setValue("Бессмертный")}>Бессмертный</li>
-                                <li className="role__name role__name--modbrb" onMouseDown={() => setValue("Доктор")}>Доктор</li>
-                                <li className="role__name role__name--modbrb" onMouseDown={() => setValue("Помошник")}>Помошник</li>
-                            </ul>)}                        
-                    </div>				                
-                    <button className="roles__add roles__add--small" onClick={() => addNewRole(value)}>Добавить</button>
-                </div>            
-                ):
-                (<button className="roles__add" onClick={()=> setAdding(true)}>+ Добавить роль</button>)
-            }
-            
-			
+			{adding ? (
+				<div className="roles__popup">
+					<div className={`roles__select ${isFocused ? 'roles__select--focus' : ''}`}>
+						<input
+							type="text"
+							value={value}
+							placeholder="Роль..."
+							className={`role__name ${isFocused ? 'role__name--modbrt' : ''}`}
+							onFocus={() => setIsFocused(!isFocused)}
+							onBlur={() => setIsFocused(false)}
+							onChange={(e) => setValue(e.target.value)}
+						/>
+						<span className="roles__close" onMouseDown={() => setValue('')}>
+							x
+						</span>
+						{isFocused && (
+							<ul className="roles__list">
+								<li
+									className="role__name role__name--modbrb"
+									onMouseDown={() => setValue('Бессмертный')}
+								>
+									Бессмертный
+								</li>
+								<li className="role__name role__name--modbrb" onMouseDown={() => setValue('Доктор')}>
+									Доктор
+								</li>
+								<li className="role__name role__name--modbrb" onMouseDown={() => setValue('Помошник')}>
+									Помошник
+								</li>
+							</ul>
+						)}
+					</div>
+					<button className="roles__add roles__add--small" onClick={() => addNewRole(value)}>
+						Добавить
+					</button>
+				</div>
+			) : (
+				<button className="roles__add" onClick={() => setAdding(true)}>
+					+ Добавить роль
+				</button>
+			)}
+
 			<button className="roles__start">Начать (10)</button>
 		</div>
 	);
