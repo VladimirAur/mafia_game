29cb3b2831378142037536519b7d20c0505ae620
unknown <vladimiraurelius@gmail.com>
2026-02-20 21:18:24 +0200

    finished slider

--- src/components/Raffle/RaffleItem.jsx
+++ src/components/Raffle/RaffleItem.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-const RaffleItem = ({nickname, role, number}) => {
-console.log(nickname, role, number);
+
+
+const RaffleItem = ({img, role, number, active, nickname, isLast, onNext, onPrev, onStart}) => {
 
 
   return (
-    <div>RaffleItem</div>
+    <div className={`raffle__item ${active ? "raffle__item--active" : ""}`}
+        style={{ backgroundImage: `url(${img})`}}>
+        <button className="raffle__btn-prev" onClick={onPrev}>prev</button>
+        <div className="raffle__top">
+            <div className="raffle__title">Игрок {number}</div>
+            <div className="raffle__player-name">{nickname}</div>
+        </div>
+        <div className="raffle__bottom">
+            <div className="raffle__player-role">{role}</div>
+            {!isLast 
+                ? (<button className="raffle__btn-next" onClick={onNext}>Следующий игрок</button>)
+                :(<button className="raffle__btn-next" onClick={onStart}>Начать игру</button>)
+            } 
+            
+        </div>        
+    </div>
   )
 }
 
-export default RaffleItem
+export default RaffleItem;
--- src/components/Raffle/index.jsx
+++ src/components/Raffle/index.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import { useAppContext } from '../../App';
 import RaffleItem from './RaffleItem';
-
+import { useNavigate } from 'react-router-dom';
 
 const Raffle = () => {
-    const { players } = useAppContext();
-  return (
-    <div className="raffle">
-        {players.map(player => (
-            <RaffleItem key={player.id} {...player}/>
-        ))}
-    </div>
-  )
-}
+	const { players } = useAppContext();
+    const navigate = useNavigate();
+	const [activeIndex, setActiveIndex] = React.useState(0);
+
+	const isFirst = activeIndex === 0;
+	const isLast = activeIndex === players.length - 1;
+
+	const nextSlide = () => {
+		if (!isLast) {
+			setActiveIndex((prev) => prev + 1);
+		}
+	};
+
+	const prevSlide = () => {
+		if (isFirst) {
+			navigate('/naming');
+		} else {
+			setActiveIndex((prev) => prev - 1);
+		}
+	};
+
+	const startGame = () => {
+		navigate('/match');
+	};
+
+	return (
+		<div className="raffle">
+			{players.map((player, index) => (
+				<RaffleItem
+					key={player.id}                    
+					{...player}
+					active={index === activeIndex}
+					isLast={isLast}
+					onNext={nextSlide}
+					onPrev={prevSlide}
+					onStart={startGame}
+				/>
+			))}
+		</div>
+	);
+};
 
 export default Raffle;
