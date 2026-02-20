44ea6729841cff799bbf5b80ba11bbb00f995f1a
unknown <vladimiraurelius@gmail.com>
2026-02-20 08:19:38 +0200

    add save localStorage

--- src/App.js
+++ src/App.js
@@ -7,8 +7,23 @@ export const AppContext = React.createContext();
 export const useAppContext = () => React.useContext(AppContext);
 
 function App() {
-	const [rolesNames, setRolesNames] = React.useState([]);
-	const [players, setPlayers] = React.useState([]);
+	const [rolesNames, setRolesNames] = React.useState(() => {
+		const saved = localStorage.getItem('rolesNames');
+		return saved ? JSON.parse(saved) : [];
+	});
+
+	const [players, setPlayers] = React.useState(() => {
+		const saved = localStorage.getItem('players');
+		return saved ? JSON.parse(saved) : [];
+	});
+
+	React.useEffect(() => {
+		localStorage.setItem('rolesNames', JSON.stringify(rolesNames));
+	}, [rolesNames]);
+
+	React.useEffect(() => {
+		localStorage.setItem('players', JSON.stringify(players));
+	}, [players]);
 
 	return (
 		<AppContext.Provider value={{ rolesNames, setRolesNames, players, setPlayers }}>
--- src/pages/Drawing.jsx
+++ src/pages/Drawing.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Drawing = () => {
   return (
-    <div>Drawing</div>
+    <div className="container">
+        <div>Drawing</div>
+    </div>
+    
   )
 }
 
