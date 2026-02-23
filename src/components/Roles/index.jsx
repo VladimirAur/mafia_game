eff84932ce35535507cdc013a7ef0aca89a09f67
unknown <vladimiraurelius@gmail.com>
2026-02-23 18:02:01 +0200

    refactor css

--- src/components/Roles/index.jsx
+++ src/components/Roles/index.jsx
@@ -5,168 +5,151 @@ import RolesItem from './RolesItem';
 import object from '../../bd.json';
 import RolesSelectItem from './RolesSelectItem';
 
-
 const Roles = () => {
 	const [roles, setRoles] = React.useState([]);
 	const [adding, setAdding] = React.useState(false);
 	const [isFocused, setIsFocused] = React.useState(false);
-	const [value, setValue] = React.useState("");
-	const {rolesNames, setRolesNames, setPlayers} = useAppContext();
-	
+	const [value, setValue] = React.useState('');
+	const { rolesNames, setRolesNames, setPlayers } = useAppContext();
 
 	React.useEffect(() => {
-        
-        const rolesArray = object.roles.map(role => ({
-            ...role,
-            id: crypto.randomUUID() 
-        }))
+		const rolesArray = object.roles.map((role) => ({
+			...role,
+			id: crypto.randomUUID(),
+		}));
 
-		setRoles(rolesArray);        
+		setRoles(rolesArray);
 	}, []);
 
-        
-	React.useEffect(() => {        
+	React.useEffect(() => {
 		const names = roles
-			.filter(role => role.number > 0)
-			.flatMap(role => Array(role.number).fill(role.name))
+			.filter((role) => role.number > 0)
+			.flatMap((role) => Array(role.number).fill(role.name))
 			.sort(() => Math.random() - 0.5);
-            
 
 		setRolesNames(names);
-        
-
 	}, [roles]);
 
-    const chooseRole = (name) => {
-        setValue(name);
-    }
+	const chooseRole = (name) => {
+		setValue(name);
+	};
 
-	const addNewRole = (value) => {        
-		const existing = roles.find(role => role.name === value);
-        let updatedRoles;
+	const addNewRole = (value) => {
+		const existing = roles.find((role) => role.name === value);
+		let updatedRoles;
 
-        if (existing){
-            updatedRoles = roles.map(role => role.name === value
-            ? {...role, number:1}
-            : role)
-        }else{
-            updatedRoles = [...roles, { name: value, number: 1 }];
-        }
+		if (existing) {
+			updatedRoles = roles.map((role) => (role.name === value ? { ...role, number: 1 } : role));
+		} else {
+			updatedRoles = [...roles, { name: value, number: 1 }];
+		}
 
-        setRoles(updatedRoles);
+		setRoles(updatedRoles);
 		setAdding(false);
 		setValue('');
-        
 	};
 
-    const incRoleNumber = (index) => {
-        setRoles(prevRoles =>
-            prevRoles.map(role => role.id === index
-                ? { ...role, number: role.number - 1 }
-                : role
-            )
-        )
-    }
-
-    const decRoleNumber = (index) => {
-        setRoles(prevRoles =>
-            prevRoles.map(role => role.id === index
-                ? { ...role, number: role.number + 1 }
-                : role
-            )
-        )
-    }
-
-	const resetRole = (index) => {
+	const incRoleNumber = (index) => {
 		setRoles((prevRoles) =>
-			prevRoles.map(
-				(role) =>
-					role.id === index
-						? { ...role, number: 0 }
-						: role, 
-			),
+			prevRoles.map((role) => (role.id === index ? { ...role, number: role.number - 1 } : role)),
 		);
 	};
-    
-    const buildPlayers = () => {
-        const playersArray = rolesNames.map((name, index) => {
-        
-        const roleFromJson = object.roles.find(
-        role => role.name === name
-        );
 
-		
-        return {
-        id: crypto.randomUUID(),
-		number: index + 1,
-        role: name,
-        nickname: "",
-        foul: 0,
-        ban: false,
-        marker: 0,
-        img: roleFromJson ? roleFromJson.img : null  
-        }
-    });
+	const decRoleNumber = (index) => {
+		setRoles((prevRoles) =>
+			prevRoles.map((role) => (role.id === index ? { ...role, number: role.number + 1 } : role)),
+		);
+	};
 
-  setPlayers(playersArray);
-};
-   	
+	const resetRole = (index) => {
+		setRoles((prevRoles) => prevRoles.map((role) => (role.id === index ? { ...role, number: 0 } : role)));
+	};
+
+	const buildPlayers = () => {
+		const playersArray = rolesNames.map((name, index) => {
+			const roleFromJson = object.roles.find((role) => role.name === name);
+
+			return {
+				id: crypto.randomUUID(),
+				number: index + 1,
+				role: name,
+				nickname: '',
+				foul: 0,
+				ban: false,
+				marker: 0,
+				img: roleFromJson ? roleFromJson.img : null,
+			};
+		});
+
+		setPlayers(playersArray);
+	};
+
 	return (
 		<div className="roles">
-			<h2 className="roles__title"># Настройки игры</h2>
-			<ul className="roles__list">
-				{roles.filter(role => role.number > 0).map((role) => (
-					<RolesItem 
-                        key={role.id} 
-                        role={role} 
-                        id={role.id} 
-                        resetRole={resetRole} 
-                        incRoleNumber={incRoleNumber}
-                        decRoleNumber={decRoleNumber}/>
-				))}
-			</ul>
-			{adding ? (
-				<div className="roles__popup">
-					<div className={`roles__select ${isFocused ? 'roles__select--focus' : ''}`}>
-						<input
-							type="text"
-							value={value}
-							placeholder="Роль..."
-							className={`role__name ${isFocused ? 'role__name--modbrt' : ''}`}
-							onFocus={() => setIsFocused(!isFocused)}
-							onBlur={() => setIsFocused(false)}
-							onChange={(e) => setValue(e.target.value)}
-						/>
-						<span className="roles__close" onMouseDown={() => setValue('')}>
-							x
-						</span>
-						{isFocused && (
-							<ul className="roles__list">
-                                {roles.filter(role => role.number === 0).map((role, index) => (
-                                    <RolesSelectItem 
-                                        key={index} 
-                                        name={role.name} 
-                                        index={index} 
-                                        chooseRole={chooseRole}
-                                    />
-                                ))}								
-							</ul>
-						)}
+			<div className="roles__options">
+				<h2 className="roles__title"># Настройки игры</h2>
+				<ul className="roles__list">
+					{roles
+						.filter((role) => role.number > 0)
+						.map((role) => (
+							<RolesItem
+								key={role.id}
+								role={role}
+								id={role.id}
+								resetRole={resetRole}
+								incRoleNumber={incRoleNumber}
+								decRoleNumber={decRoleNumber}
+							/>
+						))}
+				</ul>
+				{adding ? (
+					<div className="roles__popup">
+						<div className={`roles__select ${isFocused ? 'roles__select--focus' : ''}`}>
+							<input
+								type="text"
+								value={value}
+								placeholder="Роль..."
+								className={`role__name ${isFocused ? 'role__name--modbrt' : ''}`}
+								onFocus={() => setIsFocused(!isFocused)}
+								onBlur={() => setIsFocused(false)}
+								onChange={(e) => setValue(e.target.value)}
+							/>
+							<span className="roles__close" onMouseDown={() => setValue('')}>
+								x
+							</span>
+							{isFocused && (
+								<ul className="roles__list">
+									{roles
+										.filter((role) => role.number === 0)
+										.map((role, index) => (
+											<RolesSelectItem
+												key={index}
+												name={role.name}
+												index={index}
+												chooseRole={chooseRole}
+											/>
+										))}
+								</ul>
+							)}
+						</div>
+						<button
+							className="roles__add roles__add--small"
+							onClick={() => addNewRole(value)}
+							disabled={!value.trim()}
+						>
+							Добавить
+						</button>
 					</div>
-					<button 
-                        className="roles__add roles__add--small" 
-                        onClick={() => addNewRole(value)}
-                        disabled={!value.trim()}>
-						Добавить
+				) : (
+					<button className="roles__add" onClick={() => setAdding(true)}>
+						+ Добавить роль
 					</button>
-				</div>
-			) : (
-				<button className="roles__add" onClick={() => setAdding(true)}>
-					+ Добавить роль
-				</button>
-			)}
+				)}
+			</div>
 
-			<Link to="/naming" className="roles__start" onClick={() => buildPlayers()}>Далее ({rolesNames.length})</Link>
+			<Link to="/naming" className="roles__start" onClick={() => buildPlayers()}>
+				Далее ({rolesNames.length})
+			</Link>
 		</div>
 	);
 };
