1d5ba5af56da4c1010392f85b1d49e79424e9287
unknown <vladimiraurelius@gmail.com>
2026-02-27 16:54:23 +0200

    formating timer

--- src/components/Names/NamesItem.jsx
+++ src/components/Names/NamesItem.jsx
@@ -2,43 +2,41 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addPlayerNickname } from '../../redux/slices/playerSlice';
 
-const NamesItem = ({number}) => {
-  const dispatch = useDispatch();  
-  const players = useSelector((state) => state.players.playersData);
-  const [inFocus, setInFocus] = React.useState(false);
+const NamesItem = ({ number }) => {
+	const dispatch = useDispatch();
+	const players = useSelector((state) => state.players.playersData);
+	const [inFocus, setInFocus] = React.useState(false);
 
+	const player = players.find((player) => player.number === number);
 
-    const player = players.find(player => player.number === number);
+	const handleChange = (e) => {
+		dispatch(
+			addPlayerNickname({
+				number: player.number,
+				name: e.target.value,
+			}),
+		);
+	};
 
-    const handleChange = (e) => {
-        dispatch(addPlayerNickname({
-            number: player.number,
-            name: e.target.value
-        }))
-
-    };
- 
-
-  return (
-    <li className="player">
-        <span className="player__number">{number}</span>
-        <div className="player__desc player__desc--mod">
-          <input
-                type="text"
-                value={player?.nickname || ""}
-                placeholder="Игрок..."
-                className="player__input"
-                onFocus={() => setInFocus(true)}
-                onBlur={() => setInFocus(false)}
-                onChange={handleChange}
-            />            
-        </div>
-        {inFocus && (<button 
-                    className="role__btn role__btn--mod"                    
-                    >Готово</button>)}
-        
-    </li>
-  )
-}
+	return (
+		<li className="player">
+			<div className="player__item">
+				<span className="player__number">{number}</span>
+				<div className="player__desc player__desc--mod">
+					<input
+						type="text"
+						value={player?.nickname || ''}
+						placeholder="Игрок..."
+						className="player__input"
+						onFocus={() => setInFocus(true)}
+						onBlur={() => setInFocus(false)}
+						onChange={handleChange}
+					/>
+				</div>
+				{inFocus && <button className="role__btn role__btn--mod">Готово</button>}
+			</div>
+		</li>
+	);
+};
 
 export default NamesItem;
--- src/components/Players/PlayersItem.jsx
+++ src/components/Players/PlayersItem.jsx
@@ -1,50 +1,46 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-const PlayersItem = ({ number, nickname, role, foul, addFoul, removeFoul, excludePlayer, ban }) => {  
-    const phase = useSelector(state => state.phases.phase); 
-       
-    
+const PlayersItem = ({ number, nickname, role, foul, addFoul, removeFoul, excludePlayer, ban }) => {
+	const phase = useSelector((state) => state.phases.phase);
 
 	return (
-		<li className={`player ${ban ? "player--disabled" : ""}`} >
-			<span 
-                className={`player__number ${ban ? "player__number--disabled":""}`}
-                >{number}</span>
-			<div className="player__desc">
-				<div className="player__name">{nickname ? nickname : "Игрок"}</div>
-                {phase === 'Ночь' && (
-                    <div className="player__status">
-                        <div className="player__role">{role}</div>
-                    </div>
-                )}				
-			</div>
-            {phase === 'День' && (
-                <div className="player__foll">
-				<div className="player__foll-count">
-					<button 
-                    className="player__foll-btn player__foll-left"
-                    onClick={() => removeFoul(number)}><span className='icon-minus'></span></button>
-					<input 
-                    type="text" 
-                    value={foul > 0 ? foul : "Ф"} 
-                    className="player__foll-number"
-                    readOnly />
-					<button 
-                    className="player__foll-btn player__foll-right"
-                    onClick={() => addFoul(number)}><span className='icon-plus'></span></button>
+		<li className={`player ${ban ? 'player--disabled' : ''}`}>
+			<div className="player__item">
+				<span className={`player__number ${ban ? 'player__number--disabled' : ''}`}>{number}</span>
+				<div className="player__desc">
+					<div className="player__name">{nickname ? nickname : 'Игрок'}</div>
+					{phase === 'Ночь' && (
+						<div className="player__status">
+							<div className="player__role">{role}</div>
+						</div>
+					)}
 				</div>
+				{phase === 'День' && (
+					<div className="player__foll">
+						<div className="player__foll-count">
+							<button className="player__foll-btn player__foll-left" onClick={() => removeFoul(number)}>
+								<span className="icon-minus"></span>
+							</button>
+							<input type="text" value={foul > 0 ? foul : 'Ф'} className="player__foll-number" readOnly />
+							<button className="player__foll-btn player__foll-right" onClick={() => addFoul(number)}>
+								<span className="icon-plus"></span>
+							</button>
+						</div>
+					</div>
+				)}
+
+				<button className="player__foll-btn player__foll-del" onClick={() => excludePlayer(number)}>
+					<span className="icon-close"></span>
+				</button>
 			</div>
-            )}
-			
-            <button className="player__foll-btn player__foll-del" 
-                    onClick={() => excludePlayer(number)}><span className='icon-close'></span></button>
-            <div className="player__timer timer">
-                <div className="timer__count">60c</div>
-                <button className="timer__button">S</button>
-                <button className="timer__button">P</button>
-                <button className="timer__button">Следующий</button>
-            </div>        
+
+			<div className="player__timer timer">
+				<div className="timer__count">60c</div>
+				<button className="timer__button">S</button>
+				<button className="timer__button">P</button>
+				<button className="timer__button">Следующий</button>
+			</div>
 		</li>
 	);
 };
